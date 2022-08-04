bd569e5e3177a4a79d2922fae86deae96a4d0c38
Adelina Figueira <51302331+adelinarf@users.noreply.github.com>
2022-08-04 13:50:21 -0400

    menu que se maneja desde la consola

--- Grafo.rb
+++ Grafo.rb
@@ -19,6 +19,7 @@ class Grafo
     def agregar_arista( a, b )
         if @estructura.has_key?(a) and @estructura.has_key?(b)
             @estructura[a].append(b)
+            @estructura[b].append(a)
             return true
         else
             return false
@@ -75,7 +76,63 @@ class Grafo
     end
 end
 
-
+# NUEVOGRAFO hace reset del grafo para escribir uno nuevo sin reiniciar
+# NODO x agrega el nodo x al grafo
+# LADO x y agrega el lado x y al grafo
+# SALIR sale del ciclo y termina el programa
+# BUSCAR x y ejecuta el dfs
+def main()
+   continua = true
+   grafo = Grafo.new
+   while continua
+       puts "Introduce una entrada:"
+       b = gets.chomp
+       if b =~ /NUEVOGRAFO/
+           b.slice! "NUEVOGRAFO"
+           array = b.split(' ', -1)
+           if (array.length()>0)
+             puts "No acepta"
+           else
+             grafo = Grafo.new
+           end
+       end
+       if b =~ /NODO/
+           b.slice! "NODO"
+           array = b.split(' ', -1)
+           if (array.length()>1)
+             puts "Solo se debe insertar el nodo"
+           else
+             grafo.agregar_nodo(array[0])
+             puts "Se ha introducido el nodo "+array[0]
+           end
+       end
+       if b =~ /LADO/
+           b.slice! "LADO"
+           array = b.split(' ', -1)
+           if (array.length()>2)
+             puts "El lado solo contiene dos vertices"
+           else
+             grafo.agregar_arista(array[0], array[1])
+             puts "Se ha introducido el lado ("+array[0]+","+array[1]+")"
+           end
+       end
+       if b =~ /BUSCAR/
+           b.slice! "BUSCAR"
+           array = b.split(' ', -1)
+           if (array.length()>2)
+             puts "Solo se puede hacer la busqueda entre 2 vertices"
+           else
+             puts grafo.camino(array[0],array[1])
+           end
+       end
+       if b =~ /SALIR/
+         continua = false
+         puts "Has salido!"
+       end 
+   end
+end
+main()
+'''
 a = Grafo.new
 a.agregar_nodo("hola")
 a.agregar_nodo("mi")
@@ -88,3 +145,4 @@ a.agregar_arista("gente", "bella")
 a.agregar_arista("gente", "amable")
 a.imprimir()
 puts a.camino("hola", "amable")
+'''
