cf2ff7d9c81549a27a426c97e5c322322bbc33a4
LuisDavidGarciaLugo <107199868+LuisDavidGarciaLugo@users.noreply.github.com>
2022-08-04 21:00:14 -0400

    cambios a la consola

--- Grafo.rb
+++ Grafo.rb
@@ -87,67 +87,80 @@ end
 def main()
    continua = true
    grafo = Grafo.new
+   nodoAlmacenado = nil
+   print "se ha creado un nuevo grafo\n"
    while continua
        print "~"
        b = gets.chomp
        b.strip!
-       b.upcase!
-       if b =~ /NUEVOGRAFO/
-           b.slice! "NUEVOGRAFO"
-           array = b.split(' ', -1)
-           if (array.length()>0)
-             puts "argumento invalido: " + array[0]
+       array = b.split(' ', -1)
+       if array[0].upcase == "NUEVOGRAFO"
+           if (array.length()>1)
+             puts "argumento invalido: " + array[1]
            else
              grafo = Grafo.new
              puts "se ha creado un nuevo grafo"
            end
        
-       elsif b =~ /NODO/
-           b.slice! "NODO"
-           array = b.split(' ', -1)
-           if (array.length() == 0) 
+       elsif array[0].upcase == "NODO"
+           if (array.length() == 1) 
             puts "Falta el argumento <nodo>"
-           elsif (array.length()>1)
-            puts "Argumento invalido: " + array[1]
+           elsif (array.length() == 2) 
+            puts "Falta el argumento <informacion>"
+           elsif (array.length()>3)
+            puts "Argumento invalido: " + array[3]
            else
-             res = grafo.agregar_nodo(array[0],array[1])
+             res = grafo.agregar_nodo(array[1],array[2])
              if (res)
-               puts "Se ha introducido el nodo "+array[0]
+               puts "Se ha introducido el nodo "+array[1]
              else
-               puts "El nodo "+array[0]+" ya existe"
+               puts "El nodo "+array[1]+" ya existe"
              end
            end
        
-       elsif b =~ /LADO/
-           b.slice! "LADO"
-           array = b.split(' ', -1)
-           if (array.length()  == 0) 
+       elsif array[0].upcase == "LADO"
+           if (array.length()  == 1) 
              puts "Falta el argumento <nodo1>"
-           elsif (array.length()  == 1) 
+           elsif (array.length()  == 2) 
              puts "Falta el argumento <nodo2>"
-           elsif (array.length()>2)
-             puts "Argumento invalido: " + array[2]
+           elsif (array.length()>3)
+             puts "Argumento invalido: " + array[3]
            else
-             res = grafo.agregar_arista(array[0], array[1])
+             res = grafo.agregar_arista(array[1], array[2])
              if (res)
-               puts "Se ha introducido el lado ("+array[0]+","+array[1]+")"
+               puts "Se ha introducido el lado ("+array[1]+","+array[2]+")"
              else
-               puts "No puede introducir el lado ("+array[0]+","+array[1]+"), alguno de los nodos no existe"
+               puts "No puede introducir el lado ("+array[1]+","+array[2]+"), alguno de los nodos no existe"
              end
            end
        
-       elsif b =~ /BUSCAR/
-           b.slice! "BUSCAR"
-           array = b.split(' ', -1)
-           if (array.length == 0)
-             puts "Falta el argumento <nodo>"
-           elsif (array.length()>1)
-             puts "Argumento no reconocido: " + array[1]
-           else
-             puts grafo.camino(array[0])
-           end
-       
-       elsif b =~ /SALIR/
+       elsif array[0].upcase == "BUSCAR"
+           if (array.length == 1)
+             puts "Falta el argumento <informacion>"
+           elsif (array.length()>2)
+             puts "Argumento no reconocido: " + array[2]
+            else
+              recorrido = grafo.camino(0, array[1])
+              path =  recorrido.resume
+            if path[path.length-1].length !=0
+              nodoAlmacenado = path[path.length-1][0]
+            else
+              nodoAlmacenado = 0
+            end
+              puts path
+            end
+        elsif array[0].upcase == "ITERAR"
+          if (array.length() == 1) 
+            puts "Falta el parametro <informacion>"
+          elsif (array.length > 2)
+            puts "Argumento no reconocido: " + array[2]
+          else
+            adyacentes = grafo.adyacentes(nodoAlmacenado)
+            recorrido = grafo.camino(adyacentes[0], array[1])
+            puts recorrido.resume
+          end
+
+       elsif array[0].upcase == "SALIR"
          continua = false
          puts "Has salido!"
        else 
